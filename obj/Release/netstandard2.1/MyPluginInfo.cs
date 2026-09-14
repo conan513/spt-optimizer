c3f583b47f4230f8c5cd5f25ed12f0d2aa7bca89
ConanHUN <conanhun@hotmail.com>
2026-09-14 10:42:31 +0200

    chore: add .gitignore and untrack build artifacts

--- obj/Release/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SPTOptimizer
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SPTOptimizer";
-        public const string PLUGIN_NAME = "SPTOptimizer";
-        public const string PLUGIN_VERSION = "1.1.0";
-    }
-}
